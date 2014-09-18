5145f9b853d6c4ce0c0511e65973cee29ad4cb33
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-09-18 10:06:07 -0400

    teacher solution

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require 'bundler'
 Bundler.require
+require_relative 'lib/dog.rb'
 
 class MyApp < Sinatra::Base
 
-  
+  get '/dogs' do
+    @fido = Dog.new("Fido", 10, "Yorkie")
+    erb :dog
+  end
 
 end
---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+
+class Dog
+  attr_accessor :name, :age, :breed
+
+  # DOGS = []
+
+  def initalize(name, age, breed)
+    @name = name
+    @age = age
+    @breed = breed
+    # DOGS << self
+  end
+
+  # def self.all
+  #   DOGS
+  # end
+
+end
