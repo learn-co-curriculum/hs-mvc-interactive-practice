f78f5815c8f1a2cd3850db2fefc4eade3e60c49b
vanessadean <vanessa.dean@gmail.com>
2014-11-25 12:03:26 -0500

    goats only

--- application_controller.rb
+++ application_controller.rb
@@ -2,7 +2,6 @@ require 'bundler'
 Bundler.require
 
 require_relative './models/goat.rb'
-require_relative './models/user.rb'
 
 class MyApp < Sinatra::Base
 
@@ -13,15 +12,6 @@ class MyApp < Sinatra::Base
   get '/goat' do
     @goat1 = Goat.new("Callie",7)
     erb :goat
-  end
-
-  get '/user' do
-    @user1 = User.new("John", "John.doe@gmail.com", "May 7, 1991")
-    @user2 = User.new("Jacob", "Jacob.doe@gmail.com", "May 7, 1991")
-    @user3 = User.new("Jingle", "Jingle.doe@gmail.com", "May 7, 1991")
-    @user4 = User.new("Himer Schmidt", "Himer.doe@gmail.com", "May 7, 1991")
-    @all_the_users = User.all
-    erb :user
   end
 
 end
