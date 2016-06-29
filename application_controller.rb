a0191ba65acd393e5d06b20ab878b9d413290f30
Jeff <jolson615@gmail.com>
2016-06-29 16:31:38 -0400

    tweaks to grammar

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,20 @@
 require 'bundler'
 Bundler.require
+require_relative 'models/model.rb'
 
 class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
+  end
+
+  get '/about' do
+    erb :about
+  end
+
+  post '/results' do
+    @new_string = randomize(params[:string])
+    erb :results
   end
 
 end
